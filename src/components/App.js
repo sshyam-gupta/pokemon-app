cc240e07aad3144550617a21eb890c793299d047
Shyam Gupta <shyamsundar.g@kiprosh.com>
2018-07-19 14:00:09 +0530

    Added header

--- src/components/App.js
+++ src/components/App.js
@@ -19,6 +19,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+				<div className="header">
+					<a href="/">Home</a>
+				</div>
         <div className="center w85">
 					<div className="ph3 pv1 background-gray">
 						<Switch>
--- src/components/Pokemon/PokemonList.js
+++ src/components/Pokemon/PokemonList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Query } from 'react-apollo';
 
-import Pokemon from './Pokemon';
+import Pokemon from './Pokemon'; 
 import { GET_POKEMONS } from '../../Query/GetQuery';
 
 class PokemonList extends Component {
